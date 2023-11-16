974dc3c8fe9371abeae987feb6edf4f155d68af2
ritiksharmarj <theonlyritik@gmail.com>
2023-11-16 16:40:59 +0530

    convert modal to a compound component

--- src/features/cabins/components/AddCabin.jsx
+++ src/features/cabins/components/AddCabin.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react';
 import Modal from '../../../ui/Modal';
 import CreateCabinForm from '../CreateCabinForm';
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <>
-      <button
-        onClick={() => setIsOpenModal((show) => !show)}
-        className="rounded-md bg-brand-600 px-3 py-2 text-sm font-medium text-brand-50 shadow-sm transition-all hover:bg-brand-700"
-      >
-        Add new cabin
-      </button>
-
-      {isOpenModal && (
-        <Modal onCloseModal={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Toggle>Add new cabin</Modal.Toggle>
+      <Modal.Window>
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
 
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,39 @@
+import { cloneElement, createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { X } from '@phosphor-icons/react';
 
-function Modal({ children, onCloseModal }) {
+const ModalContext = createContext();
+
+export default function Modal({ children }) {
+  const [open, toggle] = useState(false);
+
+  const onCloseModal = () => toggle(!open);
+
+  return (
+    <ModalContext.Provider value={{ open, onCloseModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Toggle({ children }) {
+  const { onCloseModal } = useContext(ModalContext);
+
+  return (
+    <button
+      onClick={onCloseModal}
+      className="rounded-md bg-brand-600 px-3 py-2 text-sm font-medium text-brand-50 shadow-sm transition-all hover:bg-brand-700"
+    >
+      {children}
+    </button>
+  );
+}
+
+function Window({ children }) {
+  const { open, onCloseModal } = useContext(ModalContext);
+
+  if (!open) return null;
+
   return createPortal(
     <div className="fixed inset-0 z-50 bg-backdrop-color/10 backdrop-blur-sm transition-all duration-500">
       <div className="fixed left-1/2 top-1/2 z-50 w-2/4 -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-0 p-10 shadow-2xl transition-all duration-500">
@@ -12,11 +44,12 @@ function Modal({ children, onCloseModal }) {
           <X size={20} />
         </button>
 
-        {children}
+        <div>{cloneElement(children, { onCloseModal })}</div>
       </div>
     </div>,
     document.body,
   );
 }
 
-export default Modal;
+Modal.Toggle = Toggle;
+Modal.Window = Window;
