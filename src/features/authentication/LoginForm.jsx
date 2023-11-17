8697ccc4858da85045434be45dc5a613750dc24e
ritiksharmarj <theonlyritik@gmail.com>
2023-11-17 17:24:42 +0530

    auth with supabase

---
+++ src/features/authentication/LoginForm.jsx
@@ -0,0 +1,75 @@
+import { useForm } from 'react-hook-form';
+import { login } from '../../services/apiAuth';
+
+function LoginForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    login(data);
+  }
+
+  return (
+    <div className="rounded-md border border-gray-200 bg-gray-0 p-6 shadow-sm">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <div>
+          <label htmlFor="email">Email address</label>
+          <div className="mt-2">
+            <input
+              type="email"
+              id="email"
+              autoComplete="email"
+              {...register('email', {
+                required: 'Email cannot be kept empty',
+                pattern: {
+                  value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                  message: 'Email is not valid',
+                },
+              })}
+            />
+          </div>
+          {errors.email && (
+            <p className="mt-2 text-sm text-red-700">{errors.email.message}</p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <div className="mt-2">
+            <input
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              {...register('password', {
+                required: 'Password cannot be kept empty',
+                minLength: {
+                  value: 6,
+                  message: 'Must be 6 characters at least',
+                },
+              })}
+            />
+          </div>
+          {errors.password && (
+            <p className="mt-2 text-sm text-red-700">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full items-center justify-center rounded-md bg-brand-600 px-3 py-2 text-sm font-medium text-brand-50 shadow-sm transition-all hover:bg-brand-700 disabled:bg-brand-200"
+          >
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,28 @@
+import LoginForm from '../features/authentication/LoginForm';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="grid min-h-full grid-cols-2 bg-gray-50">
+      <div className="relative">
+        <img
+          src="/auth-screen-image.jpg"
+          alt="Cabin image"
+          className="h-screen w-full object-cover"
+        />
+      </div>
+
+      <div className="flex flex-col items-center justify-center px-24 py-12">
+        <div className="w-[400px]">
+          <img src="/favicon.png" alt="" className="mx-auto h-10 w-auto" />
+          <h2 className="mb-10 mt-6 text-center text-2xl font-bold leading-9">
+            Welcome to Oasis Resort
+          </h2>
+
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
