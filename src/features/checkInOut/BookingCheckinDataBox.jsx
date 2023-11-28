a68738ca7c2b41e4b1fc2401dac4b9dcd59442bf
ritiksharmarj <theonlyritik@gmail.com>
2023-11-28 17:03:17 +0530

    adding optional breakfast

--- src/features/checkInOut/BookingCheckinDataBox.jsx
+++ src/features/checkInOut/BookingCheckinDataBox.jsx
@@ -3,6 +3,7 @@ import { ArrowLeft } from '@phosphor-icons/react';
 
 import { useMoveBack } from '../../hooks/useMoveBack';
 import { useCheckin } from './hooks/useCheckin';
+import { useSettings } from '../settings/hooks/useSettings';
 import Spinner from '../../components/ui/Spinner';
 import { useBookingDetail } from '../bookings/hooks/useBookingDetail';
 import BookingDataBox from '../bookings/bookingDetails/BookingDataBox';
@@ -10,22 +11,50 @@ import { formatCurrency } from '../../utils/helpers';
 
 function BookingCheckinDataBox() {
   const [confirmPaid, setConfirmPaid] = useState(false);
+  const [addBreakfast, setAddBreakfast] = useState(false);
   const { booking, isLoading } = useBookingDetail();
-  const moveBack = useMoveBack();
   const { checkin, isCheckingIn } = useCheckin();
+  const { settings, isLoading: isLoadingSettings } = useSettings();
+
+  const moveBack = useMoveBack();
 
   // Mark check to checkbox if booking is already paid
   useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking]);
 
-  if (isLoading) return <Spinner className="mx-auto h-6 w-6" />;
+  if (isLoading || isLoadingSettings)
+    return <Spinner className="mx-auto h-6 w-6" />;
 
   // If there is no booking data to show
   if (!booking) return <div>No data to show at the moment.</div>;
 
-  const { id: bookingId, guests, totalPrice } = booking;
+  const {
+    id: bookingId,
+    guests,
+    totalPrice,
+    numGuests,
+    hasBreakfast,
+    numNights,
+  } = booking;
+
+  // Calculate total breakfast price
+  const optionalBreakfastPrice =
+    settings.breakfastPrice * numGuests * numNights;
 
   function handleCheckin() {
-    if (confirmPaid) checkin(bookingId);
+    if (!confirmPaid) return;
+
+    if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      });
+    } else {
+      checkin({ bookingId, breakfast: {} });
+    }
   }
 
   return (
@@ -45,6 +74,26 @@ function BookingCheckinDataBox() {
 
       <BookingDataBox booking={booking} />
 
+      {/* Add breakfast option if there is no */}
+      {!hasBreakfast && (
+        <div className="mt-10 flex items-center gap-2 overflow-hidden rounded-md border border-gray-200 bg-gray-0 px-10 py-5 shadow-sm">
+          <input
+            type="checkbox"
+            name="breakfast"
+            id="breakfast"
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((add) => !add);
+              setConfirmPaid(false); // Set to false if already checked before
+            }}
+            className="h-4 w-4 rounded border-gray-300 text-brand-600 focus:ring-brand-600"
+          />
+          <label htmlFor="breakfast">
+            Want to add breakfast for {formatCurrency(optionalBreakfastPrice)}?
+          </label>
+        </div>
+      )}
+
       {/* Confirm booking */}
       <div className="mt-10 flex items-center gap-2 overflow-hidden rounded-md border border-gray-200 bg-gray-0 px-10 py-5 shadow-sm">
         <input
@@ -57,7 +106,13 @@ function BookingCheckinDataBox() {
         />
         <label htmlFor="confirm">
           I confirm that {guests.name} has paid the total amount of{' '}
-          {formatCurrency(totalPrice)}
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                totalPrice + optionalBreakfastPrice,
+              )} (${formatCurrency(totalPrice)} + ${formatCurrency(
+                optionalBreakfastPrice,
+              )})`}
         </label>
       </div>
 
