76f5fae4f14024bd95b21e28d657a6e4e03deb50
ritiksharmarj <theonlyritik@gmail.com>
2023-12-03 21:24:12 +0530

    fix today activity

--- src/features/checkInOut/TodayActivity.jsx
+++ src/features/checkInOut/TodayActivity.jsx
@@ -5,20 +5,23 @@ import { useTodayActivity } from './hooks/useTodayActivity';
 function TodayActivity() {
   const { todayActivities, isLoading } = useTodayActivity();
 
-  if (isLoading) return <Spinner className="mx-auto h-6 w-6" />;
-
-  // If there is no booking data to show
-  if (!todayActivities) return <div>No activity today...</div>;
-
   return (
     <div className="col-span-2 col-start-1 flex flex-col overflow-hidden rounded-md border border-gray-200 bg-gray-0 p-6 shadow-sm">
       <div className="pb-6 text-xl font-semibold">Today activity</div>
 
-      <div className="divide-y divide-gray-200 overflow-y-auto overflow-x-hidden">
-        {todayActivities.map((activity) => (
-          <TodayActivityItem activity={activity} key={activity.id} />
-        ))}
-      </div>
+      {!isLoading ? (
+        todayActivities?.length > 0 ? (
+          <div className="divide-y divide-gray-200 overflow-y-auto overflow-x-hidden">
+            {todayActivities.map((activity) => (
+              <TodayActivityItem activity={activity} key={activity.id} />
+            ))}
+          </div>
+        ) : (
+          <div>No activity today...</div>
+        )
+      ) : (
+        <Spinner className="mx-auto h-6 w-6" />
+      )}
     </div>
   );
 }
